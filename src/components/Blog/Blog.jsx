e780e7f03d3cceae9f18b480cd31e5f78408a0a3
Naharun <naharunsraboni@gmail.com>
2023-04-01 01:22:21 +0600

    blog details added from dynamic data

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import './Blog.css'
-import logo from '../../image/programming-skills.png';
+// import logo from '../../image/programming-skills.png';
 
 
 const Blog = (props) => {
-    const {name,date,date2,img} = props.knowledge;
-    console.log(props);
+    const { name, date, time, image, author,info } = props.knowledge;
+    console.log(name);
     return (
-        <div>
-            <img className='container-image' src={logo} alt="" />
-            <div>
-                <h3>Name :{name}</h3>
+        <div className='blog-container'>
+            <img className='container-image' src={image} alt="" />
+            <div className='author-details'>
+                <div className='author-info'>
+                    <img src={author} alt="" />
+                    <div>
+                        <h3>{name}</h3>
+                        <p>{date}</p>
+                    </div>
+                </div>
+                <p>{time}</p>
             </div>
+            <h4>{info}</h4>
+            <a href="">Mark As Read</a>
         </div>
     );
 };
--- src/components/Knowledge/Knowledge.jsx
+++ src/components/Knowledge/Knowledge.jsx
@@ -3,23 +3,25 @@ import './Knowledge.css'
 import Blog from '../Blog/Blog';
 
 const Knowledge = () => {
-    const [knowledges,setKnowledge] =useState([])
-    useEffect(() =>{
+    const [knowledges, setKnowledges] = useState([]);
+    useEffect(() => {
         fetch('data.json')
-        .then(res =>res.json())
-        .then(data =>setKnowledge(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setKnowledges(data));
+    }, [])
     return (
         <div className='knowlegecafe-container'>
             <div className="knowledge-container">
                 {
                     knowledges.map(knowledge =>
-                    <Blog key ={knowledge.id} knowledge ={knowledge}>
-                    </Blog>)
+                        <Blog key={knowledge.id} knowledge={knowledge}>
+                        </Blog>)
                 }
             </div>
             <div className="time-container">
-                <h2>Spent time on read : 177 min</h2>
+                <div className='spent-time'>
+                    <h6>Spent time on read : 177 min</h6>
+                </div>
             </div>
         </div>
     );
