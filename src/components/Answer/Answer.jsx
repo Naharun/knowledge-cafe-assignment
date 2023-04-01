1dda8522d05f1d9b51f4121efcdc2825240881b0
Naharun <naharunsraboni@gmail.com>
2023-04-01 18:05:54 +0600

    added answer container

---
+++ src/components/Answer/Answer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Answer = () => {
+    return (
+        <div className='answer-container'>
+            <div>
+                <h2 className='container-title'>
+                    1.Props vs state
+                </h2>
+                <p className='container-description'>
+                    Answer :
+                </p>
+            </div>
+            <div>
+                <h2 className='container-title'>
+                    2.How does useState work?
+
+                </h2>
+                <p className='container-description'>
+                    Answer :
+                </p>
+            </div>
+            <div>
+                <h2 className='container-title'>
+                    3.Props vs state
+                </h2>
+                <p className='container-description'>
+                    Answer :
+                </p>
+            </div>
+            <div>
+                <h2 className='container-title'>
+                    4.Props vs state
+                </h2>
+                <p className='container-description'>
+                    Answer :
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Answer;
