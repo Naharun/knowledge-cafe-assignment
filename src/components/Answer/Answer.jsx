92d95707256b3337f6d6cd9d7f05a4fb5d40b83a
Naharun <naharunsraboni@gmail.com>
2023-04-01 19:43:44 +0600

    answer all the question

--- src/components/Answer/Answer.jsx
+++ src/components/Answer/Answer.jsx
@@ -8,7 +8,10 @@ const Answer = () => {
                     1.Props vs state
                 </h2>
                 <p className='container-description'>
-                    Answer :
+                    Answer :1. Props :- Props are owned and read-only by a parent
+                             State :- State is owned locally and the component updates itself.
+                            2. Props :-It can be accessed by the child component
+                               State :- It cannot be accessed by child component.
                 </p>
             </div>
             <div>
@@ -17,7 +20,10 @@ const Answer = () => {
 
                 </h2>
                 <p className='container-description'>
-                    Answer :
+                    Answer : 2. useState is react hook.It allows us to have state variable
+                                in functional components. We pass the initial to this function ,
+                                it returns a variable with the current state value and another
+                                function to update this value.
                 </p>
             </div>
             <div>
@@ -26,7 +32,11 @@ const Answer = () => {
 
                 </h2>
                 <p className='container-description'>
-                    Answer :
+                    Answer : 3. The useEffect is used for load data. useEffect takes two arguments.
+                                The first argument passed to useEffect is a function called useEffect
+                                and the second argument is an array of dependence.
+                                The useEffect in react js allows us to perform side effects in our
+                                components.
                 </p>
             </div>
             <div>
@@ -35,7 +45,10 @@ const Answer = () => {
 
                 </h2>
                 <p className='container-description'>
-                    Answer :
+                    Answer : 4. React components work to similarly to javaScript functions as they
+                                accept arbitrary inputs called Props (properties). We can use many 
+                                components above the code. ReactJs is one of the most popular javaScript
+                                libraries for mobile and web application development.
                 </p>
             </div>
         </div>
